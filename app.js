d2ae85f5301e0a4cd196c8c9767000756ca3655e
Deryabin Aleksey <deryadinalex@bk.ru>
2023-07-29 20:24:12 +0300

    fix

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
-const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect((NODE_ENV === 'production' ? DB_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb'), {
+mongoose.connect(('mongodb://127.0.0.1:27017/bitfilmsdb'), {
   useNewUrlParser: true,
 });
 
